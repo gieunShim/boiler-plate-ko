ea61f7da4ffe9e4176ea047c1a7540d777a48a32
gieunshim <gieunshim@gamil.com>
2021-05-26 15:28:01 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyparser = require('body-parser');
+
+const config = require('./config/key');
+
+const { user } = require('./models/Users');
+
+//application/x-ww-form-unlencoded
+app.use(bodyparser.urlencoded({extended: true}));
+//application/json
+app.use(bodyparser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://geshim:parckparck12!@boilerplate.wnash.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    
+    //회원가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+
+    //mongoDB에서 오는 method
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res,status(200).json({
+            success: true
+        })
+    })
+
+})
+
+
+
+
 
 app.listen(port, () => console.log(`Example app listning in port ${port}!`))
